3990fc2ea3785afde0ab57c9d2336522923d3bb7
Carlos Azuaje <carlos.azuaje@accenture.com>
2026-08-04 20:42:58 -0300

    new animation lib

--- front/src/components/CategoryCard/CategoryCard.jsx
+++ front/src/components/CategoryCard/CategoryCard.jsx
@@ -1,15 +1,13 @@
 import { Link } from 'react-router-dom'
 import { useData } from '../../context/DataContext.jsx'
-import { useScrollReveal } from '../../hooks/useScrollReveal.js'
 import styles from './CategoryCard.module.css'
 
 export default function CategoryCard({ category, index = 0 }) {
   const { products } = useData()
   const count = products.filter(p => p.categoryIds.includes(category.id)).length
-  const { ref } = useScrollReveal(index * 80)
 
   return (
-    <div ref={ref}>
+    <div data-aos="fade-up" data-aos-delay={index * 80}>
       <Link to={`/categoria/${category.slug}`} className={styles.card}>
         <div className={styles.iconWrap}>
           <span className={styles.icon}>{category.icon}</span>
